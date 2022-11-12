b4e3dea9c6b02985f8d5be868b109d1de6501cee
Mohammad <mzarei@uwaterloo.ca>
2022-11-12 10:03:08 -0500

    add crop.py

--- Crop.py
+++ Crop.py
@@ -1,38 +1,41 @@
 import os
 from PIL import Image
 
-# create a directory for cropped images
-croped_dir= "./croped_framed/"
-if not os.path.exists(croped_dir):
-    os.makedirs(croped_dir)
+
 
 path = "/content/frames/video_frames/"
 images_path = os.listdir(path)
 
+# create a directory for cropped images
+croped_dir= "../croped_framed/"
+for i in range(1,7):
+    if not os.path.exists(croped_dir + str(i) + "/"):
+        os.makedirs(croped_dir + str(i) + "/")
 
-for img_path in images_path[10:]:
+
+for img_name in images_path[:10]:
     # take the image
-    sample_image_path = path + img_path
+    sample_image_path = path + img_name
     img = Image.open(sample_image_path)
 
 
-    roi_dic = {
-        "ROI1_": [502, 344, 61, 86],
-        "ROI2_": [627, 427, 193, 93],
-        "ROI3_":  [522, 654, 90, 85],
-        "ROI4_": [704, 594, 90, 80],
-        "ROI5_": [545, 577, 81, 75],
-        "ROI6_": [460, 700, 52, 80],
-    }
+    rois = [
+        [502, 344, 61, 86],
+        [627, 427, 193, 93],
+        [522, 654, 90, 85],
+        [704, 594, 90, 80],
+        [545, 577, 81, 75],
+        [460, 700, 52, 80]
+    ]
 
-    for r in roi_dic:
+    for r in range(0, 6):
 
-        width, height = roi_dic[r][2], roi_dic[r][3]
-        x, y = roi_dic[r][0], roi_dic[r][1]
+        width, height = rois[r][2], rois[r][3]
+        x, y = rois[r][0], rois[r][1]
 
         # Select area to crop
         area = (x, y, x+width, y+height)
 
         # Crop, show, and save image
         cropped_img = img.crop(area)
-        cropped_img.save(croped_dir + r + img_path)
+        cropped_img.save(croped_dir + str(r+1) + "/"  + img_name)
