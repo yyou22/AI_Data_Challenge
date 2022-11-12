cc1696e5573d1a379b08dcd2f0a1922a06cf0347
Mohammad <mzarei@uwaterloo.ca>
2022-11-12 09:44:39 -0500

    add crop.py

--- Crop.py
+++ Crop.py
@@ -1,6 +1,11 @@
 import os
 from PIL import Image
 
+# create a directory for cropped images
+croped_dir= "./croped_framed"
+if not os.path.exists(croped_dir):
+    os.makedirs(croped_dir)
+
 path = "/content/frames/video_frames/"
 images_path = os.listdir(path)
 
@@ -12,12 +17,12 @@ for img_path in images_path:
 
 
     roi_dic = {
-        "ROI_1": [502, 344, 61, 86],
-        "ROI_2": [627, 427, 193, 93],
-        "ROI_3":  [522, 654, 90, 85],
-        "ROI_4": [704, 594, 90, 80],
-        "ROI_5": [545, 577, 81, 75],
-        "ROI_6": [460, 700, 52, 80],
+        "ROI1_": [502, 344, 61, 86],
+        "ROI2_": [627, 427, 193, 93],
+        "ROI3_":  [522, 654, 90, 85],
+        "ROI4_": [704, 594, 90, 80],
+        "ROI5_": [545, 577, 81, 75],
+        "ROI6_": [460, 700, 52, 80],
     }
 
     for r in roi_dic:
@@ -30,4 +35,4 @@ for img_path in images_path:
 
         # Crop, show, and save image
         cropped_img = img.crop(area)
-        cropped_img.save("croped_frames/"+ r +"_"+ img_path)
+        cropped_img.save(croped_dir + r + img_path)
