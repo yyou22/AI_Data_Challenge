c5c995b4ea42e8208c15528d2dcc9b66ee26beef
vyou <vyou@umich.edu>
2022-11-12 14:41:37 -0500

    finetune

--- feature_extractor.py
+++ feature_extractor.py
@@ -40,3 +40,32 @@ class ImageDataset(object):
 
     def __len__(self):
         return len(self.files)
+
+class ImageDataset_tar(object):
+    def __init__(self, folder, transforms=None):
+        self.folder = folder
+        self.transforms = transforms
+        self.files = glob.glob(os.path.join(folder, '*.jpg'))
+
+        self.csv_file_name = 'ROI1.csv'
+
+        csv_file_path = os.path.join(
+            self.folder, self.csv_file_name)
+
+        self.csv_data = pd.read_csv(csv_file_path, sep=';', usecols=["Filename", "ClassId"])
+    
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.folder, "frame_" + str(idx + 1) + ".jpg")
+        img = Image.open(img_path)
+
+        imageId = idx + 1
+
+        if self.transforms is not None:
+            img = self.transforms(img)
+
+        classId = self.csv_data.iloc[idx, 1]
+
+        return img, target, imageId
+
+    def __len__(self):
+        return len(self.files)
